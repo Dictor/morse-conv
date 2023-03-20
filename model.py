e9c70bc1db9a61fb7b8bd18caf73fbb765250be1
Jeonghyun Kim <kimdictor@gmail.com>
2023-03-20 18:15:15 +0900

    Reduce convolution total output channel as 24

--- model.py
+++ model.py
@@ -6,28 +6,28 @@ class MorseCNN(torch.nn.Module):
 
         # L1
         # input : 1 channel, 64 width, N batch
-        # after conv : 24 channel, 64 width, N batch
+        # after conv : 12 channel, 64 width, N batch
         # after pool : 24 channel, 32 width, N batch
         self.layer1 = torch.nn.Sequential(
-            torch.nn.Conv1d(1, 24, 3, padding=1),
+            torch.nn.Conv1d(1, 12, 3, padding=1),
             torch.nn.ReLU(),
             torch.nn.MaxPool1d(2, 2)
         )
 
         # L2
-        # input : 24 channel, 32 width, N batch
-        # after conv : 48 channel, 32 width, N batch
-        # after pool : 48 channel, 16 width, N batch
+        # input : 12 channel, 32 width, N batch
+        # after conv : 24 channel, 32 width, N batch
+        # after pool : 24 channel, 16 width, N batch
         self.layer2 = torch.nn.Sequential(
-            torch.nn.Conv1d(24, 48, 3, padding=1),
+            torch.nn.Conv1d(12, 24, 3, padding=1),
             torch.nn.ReLU(),
             torch.nn.MaxPool1d(2, 2)
         )
 
         # FC
-        # input : 48 channel * 16 height
+        # input : 24 channel * 16 height
         # output : 37 (alpha 26 + num 10 + other 1)
-        self.fc = torch.nn.Linear(16 * 48, 37)
+        self.fc = torch.nn.Linear(16 * 24, 37)
 
         torch.nn.init.xavier_uniform_(self.fc.weight)
 
